b40628e7ec83d771177b9ba69ae048d9879ee3a9
mostrowski <mostrowski@student.tgm.ac.at>
2017-10-02 19:53:53 +0200

    Abfrageseite der Flugnr. hinzugefügt

---
+++ Web/Web_FlugnummerSearcher.php
@@ -0,0 +1,44 @@
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		
+		<title>
+            FlugnummerSearcher
+        </title>
+		
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+    <body>
+	<br>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+				</div>
+				<div class="col-md-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h1>
+								Flugnummer Searcher
+							</h1>							
+						</div>
+						<div class="panel-body">
+							<form action="FlugData.php" method="get">
+							<input type="text" placeholder=" Insert Flightnummer" name="flugnr">
+							<button type="submit">
+								Flugnummer suchen
+							</button>
+							</form>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+		<div class="col-md-4">
+		</div>
+		
+    </body>
+</html>
